cf0903e84db73438839f5eae035adba9e2313e3e
AjangSupardi <AjangSupardi@email.com>
2023-05-25 12:32:53 +0700

    Update

--- resources/views/unify/layouts/app.blade.php
+++ resources/views/unify/layouts/app.blade.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Corporate | Unify - Multipurpose Responsive Template</title>
+    <title>Coding Sch</title>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ asset('unify') }}/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('unify') }}/csch.ico">
 
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
